34d43870607769059dc4e99b6449f581c6c3bbb9
Igor Follador <igorledf@hotmail.com>
2021-06-12 00:39:47 -0300

    :arrow_down: Remoção do socketIO

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('./database');
 const express = require('express');
 const app = express();
 const path = require('path');
-const socketIO = require('socket.io');
 
 const userRouter = require('./routes/userRouter');
 const authRouter = require('./routes/authRouter');
@@ -21,12 +20,7 @@ app.use('/auth', authRouter);
 
 app.use('/admin', adminRouter);
 
-const server = app.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log("Server running on port %s", port);
 });
 
-const io = socketIO(server);
-
-// io.on('connection', (socket) => {
-    
-// })
